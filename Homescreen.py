e90214090279fc4035eafbb769cbeb84d90ba7fc
WildBoarStudio <nonamlft@gmail.com>
2024-01-09 13:02:29 +0200

    Added background

---
+++ Homescreen.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+
+class HomeScreen:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Home Screen")
+        
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(script_dir, "Images", "background.png")
+        
+        
+        self.b_img = tk.PhotoImage(file=image_path)
+        
+        b_label = tk.Label(root, image=self.b_img)
+        b_label.place(relwidth=1, relheight=1)
+        
+        login_b = tk.Button(root, text="Login", command = self.login_click,width=13,height=2)
+        login_b.place(x=150, y=280)        
+        
+        register_b = tk.Button(root, text="Register", command = self.register_blick,width=13,height=2)
+        register_b.place(x=150, y=320)        
+
+        
+        window_width = 400
+        window_height = 500
+        self.root.geometry(f"{window_width}x{window_height}")
+
+    def login_click(self):
+        print("Button login pressed")
+    
+    def register_blick(self):
+        print("Button register clicked") 
+        
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = HomeScreen(root)
+    root.mainloop()
+    
